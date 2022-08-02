9e16a24f4ea4ab9b11cc439702841c33ed07db65
刘振 <1497689258@qq.com>
2022-08-02 19:46:30 +0800

    Site updated: 2022-08-02 19:46:29

--- js/live2d.js
+++ js/live2d.js
@@ -1,22 +1,24 @@
-if((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))){
-    //手机
-} else{
-    //电脑
-    L2Dwidget.init({
-        "model": {
-            "jsonPath": "https://unpkg.com/live2d-widget-model-miku@1.0.5/assets/miku.model.json",
-            "scale": 0.8
-        },
-        "display": {
-            "position": "left",
-            "width": 275,
-            "height": 400,
-            "hOffset": -20,
-            "vOffset": -10,
-        },
-        "mobile": {
-            "show": true,
-            "scale": 0.5
-        }
-    });
-}
+L2Dwidget.init({
+    "model": {
+        "jsonPath": "https://unpkg.com/live2d-widget-model-miku@1.0.5/assets/miku.model.json",
+        "scale": 0.8
+    },
+    "display": {
+        "position": "left",
+        "width": 275,
+        "height": 400,
+        "hOffset": -20,
+        "vOffset": -10,
+    },
+    "mobile": {
+        "show": false,
+        "scale": 0.5
+    },
+    "name": {
+        canvas: 'live2dcanvas',
+        div: 'live2d-widget',
+    },
+    "react": {
+        opacity: 1,
+    },
+});
